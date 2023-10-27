060b554f444e9c39d83d2964eddee198071a8088
Yuvraj Singh Bhatiya <yvrjbhatiya>
2023-10-27 06:33:53 +0530

    new

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,14 +1,42 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import "./Hero.css";
 
 const Hero = () => {
+  const [trendingCoins, setTrendingCoins] = useState([]);
+
   const handleButtonClick = () => {
-    const offset = 790;
+    const offset = 900;
     window.scrollBy({
       top: offset,
-      behavior: "smooth", 
+      behavior: "smooth",
     });
   };
+
+  useEffect(() => {
+    const fetchTrendingCoins = async () => {
+      try {
+        const response = await axios.get(
+          "https://api.coingecko.com/api/v3/coins/markets",
+          {
+            params: {
+              vs_currency: "usd",
+              order: "market_cap_desc",
+              per_page: 3,
+              page: 1,
+              sparkline: false,
+              locale: "en",
+            },
+          }
+        );
+        setTrendingCoins(response.data);
+      } catch (error) {
+        console.error("Error fetching trending coins: ", error);
+      }
+    };
+
+    fetchTrendingCoins();
+  }, []);
 
   return (
     <>
@@ -16,13 +44,22 @@ const Hero = () => {
         <div className="hero-content-text">
           <h1>
             Real-time
-            <br /> <span>Cryptocurrency</span>
-            <br /> <span className="span1">All in One Place!</span>
+            <br /> <span>Crypto Tracker</span>
           </h1>
         </div>
         <button className="mobile-btn-hero" onClick={handleButtonClick}>
-          Get Started
+          See Prices
         </button>
+      </div>
+
+      <div className="trending-coins-row">
+        {trendingCoins.map((coin) => (
+          <div key={coin.id} className="trending-coin-column">
+            <img src={coin.image} alt={coin.name} className="coin-image" />
+            <h2>{coin.name}</h2>
+            <p>${coin.current_price}</p>
+          </div>
+        ))}
       </div>
     </>
   );
